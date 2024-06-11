91fa0dad07cf04722a32dabad13bec4475a5ada5
ArmandoD <armandoppws@gmail.com>
2024-06-10 20:40:20 -0500

    do while loop with counter-- for login with limited attempts

--- script.js
+++ script.js
@@ -7,26 +7,46 @@ let userNotification = confirm("To see our products you have to log in") // Ask 
 if (userNotification) { 
     console.log("User Agreed to log in") // log user action if confirm == true
 } else { //user decides to not log in
-    console.log("user declined login")
+    console.log("user declined login") //log user action if confirm == false
     alert("Log in canceled, please refresh the page.")
 }
 
 return userNotification
 }
 
-let userloginconscent = userAgreementAnswer()
-console.log( "user selected " + userloginconscent)
+let userLogInConscent = userAgreementAnswer()
+console.log( "user selected " + userLogInConscent)
+
+
 
 /////  User Log in  Process
 
 let logInCounter = 3;  // user has three attempts to log in
 
 
-const userName = prompt("Username:"); // global scope so we could reuse later
-const password = prompt("Paswrod: "); // global scope
+
+do {
+
+    const userName = prompt("Username:"); 
+    const password = prompt("Paswrod: "); 
+
+    if (userName == "Client 1" && password == "pass1234") {
+
+        alert("Bienvenido " + userName)
+        console.log("User loged in successfull")
+        break
+        
+    } else {
+
+        logInCounter--
+        alert("log in failed " + logInCounter + " attempts left")
+      
+    }
+    
+} while (logInCounter >= 0 );
 
 
-function logIn (username, password) {
+/* function logIn (username, password) {
 
     if (username == "Client1" && password == "pass1234") {
 
@@ -37,17 +57,9 @@ function logIn (username, password) {
         alert("Log in credentials are wrong");
         console.log("failed.log in")
         return true
-
     }
-
-
 }
-
 logIn(userName,password)
+ */
 
 
-
-/* do {
-    let userName  = user;
-    let pass = prompt(Password)
-} while (condition); */
